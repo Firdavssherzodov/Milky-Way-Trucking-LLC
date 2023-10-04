823507efdc4568eb2c20a1880315c1e33f962d6a
firdavs <frontendhtml1234@gmail.com>
2023-10-04 16:01:46 +0500

    salom

--- src/components/Content_four.vue
+++ src/components/Content_four.vue
@@ -1,62 +1,9 @@
 <template>
   <div>
-    <!-- // -->
 
-      
-    <!-- client section start -->
-    <div class="about_section layout_padding">
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-md-6 padding_0">
-              <div class="bg_4">
-                <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-                  <div class="carousel-inner">
-                    <div class="carousel-item active">
-                      <div class="image_4"><img src="images/img-4.png"></div>
-                      <h1 class="readable_text">Readable</h1>
-                      <p class="lorem_text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less </p>
-                    </div>
-                    <div class="carousel-item">
-                      <div class="image_4"><img src="images/img-4.png"></div>
-                      <h1 class="readable_text">Readable</h1>
-                      <p class="lorem_text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less </p>
-                    </div>
-                    <div class="carousel-item">
-                      <div class="image_4"><img src="images/img-4.png"></div>
-                      <h1 class="readable_text">Readable</h1>
-                      <p class="lorem_text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less </p>
-                    </div>
-                  </div>
-                  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                    <i class="fa fa-angle-left fas" style="font-size:44px" aria-hidden="true"></i>
-                  </a>
-                  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                    <i class="fa fa-angle-right  fas" style="font-size:44px" aria-hidden="true"></i>
-                  </a>
-                </div>
-              </div>
-            </div>
-          <div class="col-md-6 padding_0">
-            <div class="bg_5  ">
-              <div class="padding_left01">
-                <h1 class="about_text">Testimonial</h1>
-                <p class="many_text1">There are many variations of passages of Lorem Ipsum available, but the majority</p>
-                <div class="read_bt1"><a href="#">Read More</a></div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-        <!-- client section start -->
-<!-- // -->
   </div>
 </template>
 
 <script setup>
 
 </script>
-
-<style   scoped>
-
-</style>
